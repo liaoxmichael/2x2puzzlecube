97f710abb6f9d0c0a4655a669ff07b0ccd987ae8
michaelxliao <109240657+michaelxliao@users.noreply.github.com>
2023-02-18 21:42:32 -0500

    Add files via upload

--- block.py
+++ block.py
@@ -8,44 +8,34 @@
     maintaining different orientations as the piece is rotated). It contains
     methods for setting those attributes.
 '''
-#####
 
 class Block:
-    neighbors = [-1, -1, -1] # default values so setNeighbor() does not have index OOB error
+    neighbors = [] # default values so setNeighbor() does not have index OOB error
     coords = [] # possible values: 0 and 1, limit length 3
     faces = [] # represented by characters, first letter of color; can rearrange in list
                # but will always remain the same 3
 
     def __init__(self, x, y, z, faceX, faceY, faceZ): # instantiates with:
-        coords = [x, y, z] # known position
-        faces = [faceX, faceY, faceZ] # known orientation
+        self.coords = [x, y, z] # known position
+        self.faces = [faceX, faceY, faceZ] # known orientation
 
-    def setNeighbor(self, block, direction): # possible issues: this has no checks; i have to
-        if direction == 'x':                 # make sure to check valid neighbor when calling
-            direction = 0                    # rotation move
-        elif direction == 'y':
-            direction = 1
-        elif direction == 'z':
-            direction = 2
-        self.neighbors[direction] = block
+    def setNeighbors(self, Xblock, Yblock, Zblock): 
+        self.neighbors = [Xblock, Yblock, Zblock]
 
     def setCoords(self, x, y, z):
-        coords = [x, y, z]
-    
-    def swapFaces(self, direction1, direction2): # when changing faces, swaps to maintain
-        if direction1 == 'x':                   # the faces. per rotation only 2 will change
-            direction1 = 0                      # orientation anyways (ex: perform frontCW() from
-        elif direction1 == 'y':                 # solved. say our block is RWB: faces are ['R', 'W', 'B']
-            direction1 = 1                      # after move, faces should now be ['W', 'R', 'B'].
-        elif direction1 == 'z':
-            direction1 = 2
-        if direction2 == 'x':
-            direction2 = 0
-        elif direction2 == 'y': # there must be a better way to do this besides passing nums
-            direction2 = 1      # note to self: try using ascii mapping, since xyz consecutive
-        elif direction2 == 'z':
-            direction2 = 2
+        self.coords = [x, y, z]
 
-        temp = self.coords[direction1] # the actual swap happens here
-        self.coords[direction1] = self.coords[direction2]
-        self.coords[direction2] = temp
+    '''
+    swapFaces exists to align faces post-rotation. Each rotation will only change 2 faces on the cubelet.
+    ex: perform frontCW() from solved. say our block is RWB: faces are ['R', 'W', 'B']
+    after move, faces should now be ['W', 'R', 'B']. Directions are 'x', 'y', or 'z'.
+    (a character, and since xyz are consecutive ASCII characters, we can use ord())
+    This allows us to change adjacent cubelets.
+    '''
+    def swapFaces(self, direction1, direction2):
+        direction1 = ord(direction1) - 120
+        direction2 = ord(direction2) - 120
+        temp = self.faces[direction1] # the actual swap happens here
+        self.faces[direction1] = self.faces[direction2]
+        self.faces[direction2] = temp                                  
+
--- cube.py
+++ cube.py
@@ -9,111 +9,310 @@
 '''
 
 import random # for scrambling
+from collections import Counter # allows for fast list equality checking, which I need to determine if solved
 from block import Block # other class
+#from sty import fg, rs # external module, to color console output (affects face characters)
 
 class Cube:
     blocks = [] # stores 8 blocks
+    actions = [] # stores all 12 action functions so can map integers 0-11
+    blocks_tuple =(Block,)
 
     def __init__(self): # creating 8 blocks and storing them
         self.reset()
 
-    def reset(): # returning the cube to a solved state
-        RWB = Block(0, 1, 0, 'R', 'W', 'B') # i 100% need to triple check this later
-        RWB.setNeighbor(PWB, 'x')
-        RWB.setNeighbor(RYB, 'y')
-        RWB.setNeighbor(RWG, 'z')
-
+    def reset(self): # returning the cube to a solved state
+        '''
+        RWB = Block(0, 1, 0, 'R4', 'W3', 'B1') # used this to make sure stickers are aligned in display
+        PWB = Block(1, 1, 0, 'P3', 'W4', 'B2')
+        PWG = Block(1, 1, 1, 'P1', 'W2', 'G4')
+        RWG = Block(0, 1, 1, 'R2', 'W1', 'G3')
+        RYG = Block(0, 0, 1, 'R1', 'Y3', 'G1')
+        PYG = Block(1, 0, 1, 'P2', 'Y4', 'G2')
+        PYB = Block(1, 0, 0, 'P4', 'Y2', 'B4')
+        RYB = Block(0, 0, 0, 'R3', 'Y1', 'B3')
+        
+        RWB = Block(0, 1, 0, fg.red + 'R' + fg.rs, fg.white + 'W' + fg.rs, fg.blue + 'B' + fg.rs)
+        PWB = Block(1, 1, 0, fg.magenta + 'P' + fg.rs, fg.white + 'W' + fg.rs, fg.blue + 'B' + fg.rs)
+        PWG = Block(1, 1, 1, fg.magenta + 'P' + fg.rs, fg.white + 'W' + fg.rs, fg.green + 'G' + fg.rs)
+        RWG = Block(0, 1, 1, fg.red + 'R' + fg.rs, fg.white + 'W' + fg.rs, fg.green + 'G' + fg.rs)
+        RYG = Block(0, 0, 1, fg.red + 'R' + fg.rs, fg.yellow + 'Y' + fg.rs, fg.green + 'G' + fg.rs)
+        PYG = Block(1, 0, 1, fg.magenta + 'P' + fg.rs, fg.yellow + 'Y' + fg.rs, fg.green + 'G' + fg.rs)
+        PYB = Block(1, 0, 0, fg.magenta + 'P' + fg.rs, fg.yellow + 'Y' + fg.rs, fg.blue + 'B' + fg.rs)
+        RYB = Block(0, 0, 0, fg.red + 'R' + fg.rs, fg.yellow + 'Y' + fg.rs, fg.blue + 'B' + fg.rs)
+        '''
+        RWB = Block(0, 1, 0, 'R', 'W', 'B') # used this to make sure stickers are aligned in display
         PWB = Block(1, 1, 0, 'P', 'W', 'B')
-        PWB.setNeighbor(RWB, 'x')
-        PWB.setNeighbor(PYB, 'y')
-        PWB.setNeighbor(PWG, 'z')
-
         PWG = Block(1, 1, 1, 'P', 'W', 'G')
-        PWG.setNeighbor(RWG, 'x')
-        PWG.setNeighbor(PYG, 'y')
-        PWG.setNeighbor(PWB, 'z')
-
         RWG = Block(0, 1, 1, 'R', 'W', 'G')
-        RWG.setNeighbor(PWG, 'x')
-        RWG.setNeighbor(RYG, 'y')
-        RWG.setNeighbor(RWB, 'z')
-
         RYG = Block(0, 0, 1, 'R', 'Y', 'G')
-        RYG.setNeighbor(PYG, 'x')
-        RYG.setNeighbor(RWG, 'y')
-        RYG.setNeighbor(RYB, 'z')
-
         PYG = Block(1, 0, 1, 'P', 'Y', 'G')
-        PYG.setNeighbor(RYG, 'x')
-        PYG.setNeighbor(PWG, 'y')
-        PYG.setNeighbor(PYB, 'z')
-
         PYB = Block(1, 0, 0, 'P', 'Y', 'B')
-        PYB.setNeighbor(RYB, 'x')
-        PYB.setNeighbor(PWB, 'y')
-        PYB.setNeighbor(PYG, 'z')
-
         RYB = Block(0, 0, 0, 'R', 'Y', 'B')
-        RYB.setNeighbor(PYB, 'x')
-        RYB.setNeighbor(RWB, 'y')
-        RYB.setNeighbor(RYG, 'z')
 
-        blocks = [RWB, PWB, PWG, RWG, RYG, PYG, PYB, RYB]
+        RWB.setNeighbors(PWB, RYB, RWG)
+        PWB.setNeighbors(RWB, PYB, PWG)
+        PWG.setNeighbors(RWG, PYG, PWB)
+        RWG.setNeighbors(PWG, RYG, RWB)
+        RYG.setNeighbors(PYG, RWG, RYB)
+        PYG.setNeighbors(RYG, PWG, PYB)
+        PYB.setNeighbors(RYB, PWB, PYG)
+        RYB.setNeighbors(PYB, RWB, RYG)
 
-    def check(): # returns a boolean value determining if the cube is solved/not
-        # might be able to do so by checking adjacent pieces? will need to test
-        # otherwise a deep check where every attribute is compared & adjusted for
-        # potential orientation shifts (cube that is solved but rotated along XY
-        # plane should pass test!)
-        return True or False 
+        self.blocks = [RYB, RYG, RWB, RWG, PYB, PYG, PWB, PWG] # 000 001 010 011 100 101 110 111
+        self.blocks_tuple = (RYB, RYG, RWB, RWG, PYB, PYG, PWB, PWG) # saving as tuple for checking
+        # binary representation can also encode xyz coords
 
-    def scramble(k): # performs k quarter-turn moves on the cube to randomize it
-        pass # remember to ensure that subsequent random moves don't directly undo prev move
+    '''
+    check returns a boolean value determining if the cube is solved/not
+    might be able to do so by checking adjacent pieces? will need to test
+    otherwise a deep check where every attribute is compared & adjusted for
+    potential orientation shifts (cube that is solved but rotated along XY
+    plane should pass test!)
+    '''
+    def check(self): # if these two opposite cubelets (in solved state) have proper neighbors, then cube is solved
+        return Counter(self.blocks_tuple[0].neighbors) == Counter([self.blocks_tuple[4], self.blocks_tuple[2], self.blocks_tuple[1]]) and Counter(self.blocks_tuple[7].neighbors) == Counter([self.blocks_tuple[3], self.blocks_tuple[5], self.blocks_tuple[6]])
 
-    def display(): # possibly will use graphics library from CS1:
-        # plan to add interactable buttons that will call methods here
-        # essentially the GUI for a user to solve on
-        pass
+    def scramble(self, k): # performs k quarter-turn moves on the cube to randomize it
+        random_action = random.randint(0,11)
+        for i in range(k):
+            prev_action = random_action
+            random_action = random.randint(0,11)
+            while random_action == (prev_action + 6) % 12: # +6 % 12 is the u-turn move so we must avoid it
+                random_action = random.randint(0,11)
+            self.actions[random_action](self)
+            # print(self.actions[random_action]) # debug code to make sure we're not getting u-turns
 
+    def display(self): # for some reason graphics library won't run in vscode so: instead,
+        # we should have a running menu in console that takes in data to run commands
+        Bface = [['',''],['','']]
+        Lface = [['',''],['','']]
+        Uface = [['',''],['','']]
+        Rface = [['',''],['','']]
+        Fface = [['',''],['','']]
+        Dface = [['',''],['','']]
+        middle_faces = [Lface, Uface, Rface]
+        for block in self.blocks:
+            if block.coords[2] == 1: # face B
+                Bface[block.coords[1]][block.coords[0]] = block.faces[2]
+            if block.coords[0] == 0: # face L
+                Lface[(block.coords[2]+1)%2][block.coords[1]] = block.faces[0]
+            if block.coords[1] == 1: # face U
+                Uface[(block.coords[2]+1)%2][block.coords[0]] = block.faces[1]
+            if block.coords[0] == 1: # face R
+                Rface[(block.coords[2]+1)%2][(block.coords[1]+1)%2] = block.faces[0]
+            if block.coords[2] == 0: # face F
+                Fface[(block.coords[1]+1)%2][block.coords[0]] = block.faces[2]
+            if block.coords[1] == 0: # face D
+                Dface[block.coords[2]][block.coords[0]] = block.faces[1]
+        top = "    " + Bface[0][0] + ' ' + Bface[0][1] + '\n    ' + Bface[1][0] + ' ' + Bface[1][1] + '\n' 
+        middle_top = ""
+        middle_bottom = ""
+        for face in middle_faces:
+            for i in range(2):
+                if i == 0:
+                    middle_top += face[i][0] + ' ' + face[i][1] + ' '
+                else:
+                    middle_bottom += face[i][0] + ' ' + face[i][1] + ' '
+        bottom = "    " + Fface[0][0] + ' ' + Fface[0][1] + '\n    ' + Fface[1][0] + ' ' + Fface[1][1] + '\n    ' + Dface[0][0] + ' ' + Dface[0][1] + '\n    ' + Dface[1][0] + ' ' + Dface[1][1] + '\n-----------' 
+        print(top + middle_top + '\n' + middle_bottom + '\n' + bottom)
+    
+    def swapBlocks(self, pos1, pos2, pos3, pos4): # moves all the blocks around in the list by 4, takes in strings of binary representing positions in blocks and the cube
+        temp = self.blocks[int(pos4,2)]
+        self.blocks[int(pos4,2)] = self.blocks[int(pos3,2)]
+        self.blocks[int(pos3,2)] = self.blocks[int(pos2,2)]
+        self.blocks[int(pos2,2)] = self.blocks[int(pos1,2)]
+        self.blocks[int(pos1,2)] = temp
+
+    def updateNeighbors(self): # whenever a rotation is made, all cubelets must update adjacent blocks
+        for block in self.blocks:
+            newX = "{}{}{}".format((block.coords[0] + 1)%2, block.coords[1], block.coords[2])
+            newY = "{}{}{}".format(block.coords[0], (block.coords[1] + 1)%2, block.coords[2])
+            newZ = "{}{}{}".format(block.coords[0], block.coords[1], (block.coords[2] + 1)%2)
+            block.setNeighbors(self.blocks[int(newX,2)], self.blocks[int(newY,2)], self.blocks[int(newZ,2)])
     '''
     The following 8 functions are rotations, concerning the face and whether it is
-    clockwise (CW) or counter-clockwise (CCW). These are all quarter-turns. Implementation
-    will be tricky, but as long as I check my math and assignments of neighbors/orientations
-    /coordinates, it should turn out fine. There may be a formula I can follow to rotate points
-    in 3D space – bears looking into to minimize human error.
+    clockwise (CW) or counter-clockwise (CCW). These are all quarter-turns.
+    The comments indicate the order of coordinates being changed (and the standard puzzle cube notation for that turn).
     '''
-    def frontCW():
-        pass
-    
-    def frontCCW():
-        pass
+    def frontCW(self): # 010 -> 110 -> 100 -> 000 -> 010 , F
+        self.swapBlocks("010","110","100","000")
+        for block in self.blocks:
+            if block.coords[2] == 0: # face F
+                if block.coords[0] == 0 and block.coords[1] == 1: # all neighbors are flipped bit!
+                    block.setCoords(1,1,0)
+                elif block.coords[0] == 1 and block.coords[1] == 1:
+                    block.setCoords(1,0,0)
+                elif block.coords[0] == 1 and block.coords[1] == 0:
+                    block.setCoords(0,0,0)
+                elif block.coords[0] == 0 and block.coords[1] == 0:
+                    block.setCoords(0,1,0)
+                block.swapFaces('x', 'y') # due to 90 deg rotation, xy swap on all cubelets moved
+        self.updateNeighbors()
+        
+    def frontCCW(self): # 010 <- 110 <- 100 <- 000 <- 010, F'
+        self.swapBlocks("000","100","110","010")
+        for block in self.blocks:
+            if block.coords[2] == 0: # face F
+                if block.coords[0] == 0 and block.coords[1] == 1:
+                    block.setCoords(0,0,0)
+                elif block.coords[0] == 1 and block.coords[1] == 1:
+                    block.setCoords(0,1,0)
+                elif block.coords[0] == 1 and block.coords[1] == 0:
+                    block.setCoords(1,1,0)
+                elif block.coords[0] == 0 and block.coords[1] == 0:
+                    block.setCoords(1,0,0)
+                block.swapFaces('x', 'y') # due to 90 deg rotation, xy swap on all cubelets moved
+        self.updateNeighbors()
 
-    def backCW():
-        pass
+    def backCW(self): # 011 -> 001 -> 101 -> 111 -> 011, B
+        self.swapBlocks("011","001","101","111")
+        for block in self.blocks:
+            if block.coords[2] == 1: # face B
+                if block.coords[0] == 0 and block.coords[1] == 1:
+                    block.setCoords(0,0,1)
+                elif block.coords[0] == 0 and block.coords[1] == 0:
+                    block.setCoords(1,0,1)
+                elif block.coords[0] == 1 and block.coords[1] == 0:
+                    block.setCoords(1,1,1)
+                elif block.coords[0] == 1 and block.coords[1] == 1:
+                    block.setCoords(0,1,1)
+                block.swapFaces('x', 'y') # due to 90 deg rotation, xy swap on all cubelets moved
+        self.updateNeighbors()
 
-    def backCCW():
-        pass
+    def backCCW(self): # 011 <- 001 <- 101 <- 111 <- 011, B'
+        self.swapBlocks("111","101","001","011")
+        for block in self.blocks:
+            if block.coords[2] == 1: # face B
+                if block.coords[0] == 0 and block.coords[1] == 1:
+                    block.setCoords(1,1,1)
+                elif block.coords[0] == 0 and block.coords[1] == 0:
+                    block.setCoords(0,1,1)
+                elif block.coords[0] == 1 and block.coords[1] == 0:
+                    block.setCoords(0,0,1)
+                elif block.coords[0] == 1 and block.coords[1] == 1:
+                    block.setCoords(1,0,1)
+                block.swapFaces('x', 'y') # due to 90 deg rotation, xy swap on all cubelets moved
+        self.updateNeighbors()
 
-    def rightCW():
-        pass
+    def rightCW(self): # 110 -> 111 -> 101 -> 100 -> 110, R
+        self.swapBlocks("110","111","101","100")
+        for block in self.blocks:
+            if block.coords[0] == 1: # face R
+                if block.coords[1] == 1 and block.coords[2] == 0:
+                    block.setCoords(1,1,1)
+                elif block.coords[1] == 1 and block.coords[2] == 1:
+                    block.setCoords(1,0,1)
+                elif block.coords[1] == 0 and block.coords[2] == 1:
+                    block.setCoords(1,0,0)
+                elif block.coords[1] == 0 and block.coords[2] == 0:
+                    block.setCoords(1,1,0)
+                block.swapFaces('y', 'z') # due to 90 deg rotation, yz swap on all cubelets moved
+        self.updateNeighbors()
 
-    def rightCCW():
-        pass
+    def rightCCW(self): # 110 <- 111 <- 101 <- 100 <- 110, R'
+        self.swapBlocks("100","101","111","110")
+        for block in self.blocks:
+            if block.coords[0] == 1: # face R
+                if block.coords[1] == 1 and block.coords[2] == 0:
+                    block.setCoords(1,0,0)
+                elif block.coords[1] == 1 and block.coords[2] == 1:
+                    block.setCoords(1,1,0)
+                elif block.coords[1] == 0 and block.coords[2] == 1:
+                    block.setCoords(1,1,1)
+                elif block.coords[1] == 0 and block.coords[2] == 0:
+                    block.setCoords(1,0,1)
+                block.swapFaces('y', 'z') # due to 90 deg rotation, yz swap on all cubelets moved
+        self.updateNeighbors()
 
-    def leftCW():
-        pass
+    def leftCW(self): # 010 -> 000 -> 001 -> 011 -> 010, L
+        self.swapBlocks("010","000","001","011")
+        for block in self.blocks:
+            if block.coords[0] == 0: # face L
+                if block.coords[1] == 1 and block.coords[2] == 0:
+                    block.setCoords(0,0,0)
+                elif block.coords[1] == 0 and block.coords[2] == 0:
+                    block.setCoords(0,0,1)
+                elif block.coords[1] == 0 and block.coords[2] == 1:
+                    block.setCoords(0,1,1)
+                elif block.coords[1] == 1 and block.coords[2] == 1:
+                    block.setCoords(0,1,0)
+                block.swapFaces('y', 'z') # due to 90 deg rotation, yz swap on all cubelets moved
+        self.updateNeighbors()
 
-    def leftCCW():
-        pass
+    def leftCCW(self): # 010 <- 000 <- 001 <- 011 <- 010, L'
+        self.swapBlocks("011","001","000","010")
+        for block in self.blocks:
+            if block.coords[0] == 0: # face L
+                if block.coords[1] == 1 and block.coords[2] == 0:
+                    block.setCoords(0,1,1)
+                elif block.coords[1] == 0 and block.coords[2] == 0:
+                    block.setCoords(0,1,0)
+                elif block.coords[1] == 0 and block.coords[2] == 1:
+                    block.setCoords(0,0,0)
+                elif block.coords[1] == 1 and block.coords[2] == 1:
+                    block.setCoords(0,0,1)
+                block.swapFaces('y', 'z') # due to 90 deg rotation, yz swap on all cubelets moved
+        self.updateNeighbors()
 
-    def topCW():
-        pass
+    def topCW(self): # 010 -> 011 -> 111 -> 110 -> 010, U
+        self.swapBlocks("010","011","111","110")
+        for block in self.blocks:
+            if block.coords[1] == 1: # face U
+                if block.coords[0] == 0 and block.coords[2] == 0:
+                    block.setCoords(0,1,1)
+                elif block.coords[0] == 0 and block.coords[2] == 1:
+                    block.setCoords(1,1,1)
+                elif block.coords[0] == 1 and block.coords[2] == 1:
+                    block.setCoords(1,1,0)
+                elif block.coords[0] == 1 and block.coords[2] == 0:
+                    block.setCoords(0,1,0)
+                block.swapFaces('x', 'z') # due to 90 deg rotation, yz swap on all cubelets moved
+        self.updateNeighbors()
 
-    def topCCW():
-        pass
+    def topCCW(self): # 010 <- 011 <- 111 <- 110 <- 010, U'
+        self.swapBlocks("110","111","011","010")
+        for block in self.blocks:
+            if block.coords[1] == 1: # face U
+                if block.coords[0] == 0 and block.coords[2] == 0:
+                    block.setCoords(1,1,0)
+                elif block.coords[0] == 0 and block.coords[2] == 1:
+                    block.setCoords(0,1,0)
+                elif block.coords[0] == 1 and block.coords[2] == 1:
+                    block.setCoords(0,1,1)
+                elif block.coords[0] == 1 and block.coords[2] == 0:
+                    block.setCoords(1,1,1)
+                block.swapFaces('x', 'z') # due to 90 deg rotation, yz swap on all cubelets moved
+        self.updateNeighbors()
 
-    def bottomCW():
-        pass
+    def bottomCW(self): # 000 -> 100 -> 101 -> 001 -> 000, D
+        self.swapBlocks("000","100","101","001")
+        for block in self.blocks:
+            if block.coords[1] == 0: # face D
+                if block.coords[0] == 0 and block.coords[2] == 0:
+                    block.setCoords(1,0,0)
+                elif block.coords[0] == 1 and block.coords[2] == 0:
+                    block.setCoords(1,0,1)
+                elif block.coords[0] == 1 and block.coords[2] == 1:
+                    block.setCoords(0,0,1)
+                elif block.coords[0] == 0 and block.coords[2] == 1:
+                    block.setCoords(0,0,0)
+                block.swapFaces('x', 'z') # due to 90 deg rotation, yz swap on all cubelets moved
+        self.updateNeighbors()
 
-    def bottomCCW():
-        pass
+    def bottomCCW(self): # 000 <- 100 <- 101 <- 001 <- 000, D'
+        self.swapBlocks("001","101","100","000")
+        for block in self.blocks:
+            if block.coords[1] == 0: # face D
+                if block.coords[0] == 0 and block.coords[2] == 0:
+                    block.setCoords(0,0,1)
+                elif block.coords[0] == 1 and block.coords[2] == 0:
+                    block.setCoords(0,0,0)
+                elif block.coords[0] == 1 and block.coords[2] == 1:
+                    block.setCoords(1,0,0)
+                elif block.coords[0] == 0 and block.coords[2] == 1:
+                    block.setCoords(1,0,1)
+                block.swapFaces('x', 'z') # due to 90 deg rotation, yz swap on all cubelets moved
+        self.updateNeighbors()
+
+    actions = [frontCW, backCW, rightCW, leftCW, topCW, bottomCW, frontCCW,  backCCW, rightCCW,  leftCCW, topCCW, bottomCCW] # list created to
